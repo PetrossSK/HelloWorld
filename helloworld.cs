4c3fada68ecde9a34665d33261c683f4f761c2d1
PetrossSK <petross.original@gmail.com>
2023-12-08 15:46:42 +0100

    cw lines modified

--- helloworld.cs
+++ helloworld.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,9 @@ namespace HelloWorldcode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            global::System.Console.WriteLine("Hello World!");
+            global::System.Console.WriteLine("GitHub is working!");
+            
         }
     }
 }
